d0d0d85b5a82e275ec2a502fb537471035e78879
mahendarKa <mahendar.k@hansetech.in>
2026-07-05 18:50:21 +0530

    Update axiosConfig.js

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080/api"
+    baseURL: process.env.REACT_APP_API_URL
 });
 
 // Flag to prevent multiple redirects
